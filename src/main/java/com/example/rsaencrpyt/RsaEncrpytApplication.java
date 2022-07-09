b0695029248d0d2059345b3228bf9468bc93de5e
이진원 <ljw0829@midasit.com>
2022-07-10 00:45:49 +0900

    - feat : rsa encrypt 초기 설정

---
+++ src/main/java/com/example/rsaencrpyt/RsaEncrpytApplication.java
@@ -0,0 +1,13 @@
+package com.example.rsaencrpyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsaEncrpytApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsaEncrpytApplication.class, args);
+    }
+
+}
